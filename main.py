c10cf7e73dadcf0bbf8361f29455d742ef71d1f6
taqei <taki.djaidja@u-bourgogne.fr>
2022-02-24 19:31:20 +0100

    readfile oK

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-REGEX= "(\d) (\d)"
-re.match(REGEX,t).group(1)
+# REGEX= "(\d) (\d)"
+# re.match(REGEX,t).group(1)
 import re
 
 from scipy import rand 
 
 def read_file(path):
     cs = {}
+    projects = {}
     with open(path) as infile:
         C,P = re.match("(\d+) (\d+)",infile.readline()).group(1,2)
-
+     
         counter = 0
-        while counter < C :
+        while counter < int(C) :
             name,n_skills = re.match("(\w+) (\d+)",infile.readline()).group(1,2)
             skills = {}
-            for i in rand(n_skills):
+            for i in range(int(n_skills)):
                 lang,level = re.match("(.+) (\d+)",infile.readline()).group(1,2)
-                skills[lang]=level
+                skills[lang]=int(level)
             
             cs[name]=skills
             counter +=1
-    
+
         counter = 0
-        
-        #for end in simfile: pass 
-    
+        while counter < int(P) :
+            regex = "([\w|\d]+) (\d+) (\d+) (\d+) (\d+)"
+            name,D,S,B,R = re.match(regex,infile.readline()).group(1,2,3,4,5)
+            roles={}
+            for i in range(int(R)):
+                lang,level = re.match("(.+) (\d+)",infile.readline()).group(1,2)
+                roles[lang]=int(level)
+            projects[name]={
+                "D" : int(D),
+                "B" : int(B),
+                "R" : int(R),
+                "roles" : roles
+            }
+            counter +=1
+
+    return cs,projects
+  
+
+read_file("a_an_example.in.txt")
